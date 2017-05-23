2a5ddf35c3b19076a3bbaab57f38a0ff8283fa7e
jesseakt <thorenje@oregonstate.edu>
2017-05-23 00:44:43 -0700

    First Commit

---
+++ project6AM.cpp
@@ -0,0 +1,9 @@
+/***
+ * CS 475 Project 6: Array Multiply & Array Multiply-Add
+ * Date: May 23, 2017
+ * Description: Used to compare runtime of an array multiply (add) operation
+ *    using openCL and different local/global work sizes.
+ * References: web.engr.oregonstate.edu/~mjb/cs575/Projects/proj06.html
+ * **/
+
+
